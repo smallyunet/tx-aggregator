b0c0d26454aecb13797e494e397ac9d0f5865b95
smallyunet <dev.wangyu@gmail.com>
2025-04-29 09:50:25 +0800

    use valkey replace redis
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- updateConfig.sh
+++ updateConfig.sh
@@ -2,7 +2,7 @@
 
 # dev
 curl --request PUT \
-  --data-binary @config/config.dev.yaml \
+  --data-binary @configfiles/config.dev.yaml \
   http://127.0.0.1:8500/v1/kv/config/tx-aggregator/dev
 
 curl http://127.0.0.1:8500/v1/kv/config/tx-aggregator/dev
@@ -10,7 +10,7 @@ curl http://127.0.0.1:8500/v1/kv/config/tx-aggregator/dev
 
 # test
 curl --request PUT \
-  --data-binary @config/config.test.yaml \
+  --data-binary @configfiles/config.test.yaml \
   http://10.234.10.222:8501/v1/kv/config/tx-aggregator/test
 
 curl http://10.234.10.222:8501/v1/kv/config/tx-aggregator/test
