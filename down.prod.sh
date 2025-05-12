21cfbaffc42224984ae0c6474ca6c9ded889cb07
smallyunet <dev.wangyu@gmail.com>
2025-05-12 19:17:26 +0800

    use ip+port as consul instance id
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- down.prod.sh
+++ down.prod.sh
@@ -1,2 +1,2 @@
-docker compose --env-file .env.prod.c20 -f docker-compose.yml
-docker compose --env-file .env.prod.c21 -f docker-compose.yml
+docker compose --env-file .env.prod.c20 -f docker-compose.yml down
+docker compose --env-file .env.prod.c21 -f docker-compose.yml down
